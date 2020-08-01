082203eecfcd6246199e2b5c92d10e443a9336ff
hafizeakyildiz <68875849+hafizeakyildiz@users.noreply.github.com>
2020-08-01 16:38:27 -0400

    Update ILoveJava.java

--- src/ILoveJava.java
+++ src/ILoveJava.java
@@ -2,11 +2,13 @@ public class ILoveJava {
     public static void main(String[] args) {
         byte a =1;
         byte b =3;
+        byte c =3;
         if (a >b){
             System.out.println(false);
         }else{
             System.out.println(true);
+        }else{
+            System.out.println(c);
         }
-
     }
 }
