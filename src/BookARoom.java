5de790ce276562981a8b181d791745d32a12563b
hafizeakyildiz <hafizeakyildiz1@gmail.com>
2020-08-01 15:37:28 -0400

    add BookARoom.java

---
+++ src/BookARoom.java
@@ -0,0 +1,16 @@
+public class BookARoom {
+    public static void main(String[] args) {
+     int a = 100;
+     int b = 200;
+        boolean result = a > b;
+
+        System.out.println(result);
+
+
+
+
+
+
+
+    }
+}
