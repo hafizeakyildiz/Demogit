3f53aa7452226a1204449dafc71e0b802f87b7ce
hafizeakyildiz <hafizeakyildiz1@gmail.com>
2020-08-01 15:51:38 -0400

    add ILoveJava

---
+++ src/ILoveJava.java
@@ -0,0 +1,12 @@
+public class ILoveJava {
+    public static void main(String[] args) {
+        byte a =1;
+        byte b =3;
+        if (a >b){
+            System.out.println(false);
+        }else{
+            System.out.println(true);
+        }
+
+    }
+}
